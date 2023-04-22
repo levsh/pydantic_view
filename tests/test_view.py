e13f2d00772955b282f923a26aa210dcd15c5c88
Roman Koshel <roma.koshel@gmail.com>
2023-04-22 19:30:03 +0300

    Init

---
+++ tests/test_view.py
@@ -0,0 +1,129 @@
+from typing import List, Optional, Tuple
+
+import pytest
+from pydantic import BaseModel, Field, ValidationError
+from pydantic_view import view
+
+
+@view("ViewA", exclude=["aa", "dd"])
+@view("ViewR", include=["cc"])
+class SubModel(BaseModel):
+    aa: int
+    bb: str
+    cc: List[int]
+    dd: int = None
+    ee: Optional[str]
+
+
+@view("ViewA", exclude=["a", "d"])
+@view("ViewB", exclude=["c"], optional=["b"])
+@view("ViewC", include=["a"])
+@view("ViewR", include=["c"], recursive=True)
+@view("ViewO", include=["a", "b"], optional=["a"], optional_ex={"b": Field(default_factory=lambda: "B")})
+class Model(BaseModel):
+    a: int
+    b: str
+    c: Tuple[SubModel, SubModel]
+    d: int = None
+    e: Optional[str]
+
+
+def test_model():
+    assert hasattr(Model, "ViewA")
+    assert hasattr(Model, "ViewB")
+    assert issubclass(Model.ViewA, Model)
+    assert issubclass(Model.ViewA, BaseModel)
+    assert issubclass(Model.ViewB, Model)
+    assert issubclass(Model.ViewB, BaseModel)
+    assert issubclass(Model.ViewC, Model)
+    assert issubclass(Model.ViewC, BaseModel)
+    assert Model.ViewA != Model.ViewB != Model.ViewC
+
+    with pytest.raises(ValidationError):
+        Model()
+
+    model = Model(a=0, b="b", c=[SubModel(aa=1, bb="bb", cc=[1, 2]), {"aa": 2, "bb": "BB", "cc": [3, 4]}])
+
+    assert issubclass(Model, BaseModel)
+    assert isinstance(model, Model)
+    assert isinstance(model, BaseModel)
+    assert model.a == 0
+    assert model.b == "b"
+    assert model.c == (SubModel(aa=1, bb="bb", cc=[1, 2]), SubModel(aa=2, bb="BB", cc=[3, 4]))
+    assert model.d is None
+    assert model.e is None
+
+    assert hasattr(model, "ViewA")
+    assert hasattr(model, "ViewB")
+    assert hasattr(model, "ViewC")
+    assert issubclass(model.ViewA, Model)
+    assert issubclass(model.ViewA, BaseModel)
+    assert issubclass(model.ViewB, Model)
+    assert issubclass(model.ViewB, BaseModel)
+    assert issubclass(model.ViewC, Model)
+    assert issubclass(model.ViewC, BaseModel)
+    assert model.ViewA != model.ViewB != model.ViewC
+
+    view_a = model.ViewA()
+    assert isinstance(view_a, model.ViewA)
+    assert isinstance(view_a, BaseModel)
+    assert not hasattr(view_a, "a")
+    assert not hasattr(view_a, "d")
+    assert view_a.b == "b"
+    assert view_a.c == (SubModel(aa=1, bb="bb", cc=[1, 2]), SubModel(aa=2, bb="BB", cc=[3, 4]))
+    assert view_a.e is None
+    assert view_a.dict() == {
+        "b": "b",
+        "c": (
+            {"aa": 1, "bb": "bb", "cc": [1, 2], "dd": None, "ee": None},
+            {"aa": 2, "bb": "BB", "cc": [3, 4], "dd": None, "ee": None},
+        ),
+        "e": None,
+    }
+
+    with pytest.raises(ValidationError):
+        Model.ViewB()
+
+    view_b = Model.ViewB(a=0, e="e")
+    assert view_b.a == 0
+    assert view_b.b is None
+    assert not hasattr(view_b, "c")
+    assert view_b.d is None
+    assert view_b.e == "e"
+
+    view_c = model.ViewC()
+    assert view_c.a == 0
+    assert not hasattr(view_c, "b")
+    assert not hasattr(view_c, "c")
+    assert not hasattr(view_c, "d")
+    assert not hasattr(view_c, "e")
+
+    view_r = model.ViewR()
+    assert not hasattr(view_r, "a")
+    assert not hasattr(view_r, "b")
+    assert not hasattr(view_r, "d")
+    assert not hasattr(view_r, "e")
+    assert not hasattr(view_r.c[0], "aa")
+    assert not hasattr(view_r.c[0], "bb")
+    assert not hasattr(view_r.c[0], "dd")
+    assert not hasattr(view_r.c[0], "ee")
+    assert view_r.c[0].cc == [1, 2]
+    assert not hasattr(view_r.c[1], "aa")
+    assert not hasattr(view_r.c[1], "bb")
+    assert not hasattr(view_r.c[1], "dd")
+    assert not hasattr(view_r.c[1], "ee")
+    assert view_r.c[1].cc == [3, 4]
+
+    view_o = model.ViewO()
+    assert view_o.a == 0
+    assert view_o.b == "b"
+    assert not hasattr(view_o, "c")
+    assert not hasattr(view_o, "d")
+    assert not hasattr(view_o, "e")
+
+    view_o = Model.ViewO()
+    assert view_o.a is None
+    assert view_o.b == "B"
+    assert not hasattr(view_o, "c")
+    assert not hasattr(view_o, "d")
+    assert not hasattr(view_o, "e")
